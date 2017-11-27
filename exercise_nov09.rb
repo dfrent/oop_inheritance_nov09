b63ab0b57191ae82808ff5f103f44084e2263ec9
cordell <cordell.perry@gmail.com>
2017-11-27 14:43:27 -0500

    inheritance chain explination

--- exercise_nov09.rb
+++ exercise_nov09.rb
@@ -7,3 +7,8 @@ chris = Student.new("kevin")
 
 puts mina.teach
 puts kevin.learn
+
+
+# mina won't work because the instructor class inherits from the
+# person class. Neither Instructor or Person have the learn method, so
+# an Instructor won't be able to learn.
